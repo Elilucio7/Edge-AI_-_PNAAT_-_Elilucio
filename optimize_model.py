29392d07bf1d11be9a12c72b8d92ea1e0cf024b1
Elilúcio <eliluciofilho@gmail.com>
2026-04-22 17:37:37 +0000

    fix optimize_model agora com base em model.h5

--- optimize_model.py
+++ optimize_model.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import os
 
-model = tf.keras.models.load_model("model.keras")
+model = tf.keras.models.load_model("model.h5")
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
 tflite_d = converter.convert()
