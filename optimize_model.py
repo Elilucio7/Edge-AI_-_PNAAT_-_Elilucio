ce4108aa064806db56f7b4c735c3522d932d07b1
Elilúcio <eliluciofilho@gmail.com>
2026-04-26 18:14:06 +0000

    comentario para conversao

--- optimize_model.py
+++ optimize_model.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+#conversão de .h5 para .tflite
 model = tf.keras.models.load_model("model.h5")
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
